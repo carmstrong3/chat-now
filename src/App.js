0b4bfef263bdeb7a3ea2872525f491b6d45e30f5
Clifford Armstrong III <clifford.armstrong.3@gmail.com>
2018-09-17 10:13:20 -0700

    getting error message 'You should not use <Route> or withRouter() outside a <Router>'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 import './App.css';
+
+<script src="https://www.gstatic.com/firebasejs/5.5.0/firebase.js"></script>
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCgQovkY_pfDRA_C7YVReNC1Fj2IkfA1wU",
+  authDomain: "chat-now-72b22.firebaseapp.com",
+  databaseURL: "https://chat-now-72b22.firebaseio.com",
+  projectId: "chat-now-72b22",
+  storageBucket: "chat-now-72b22.appspot.com",
+  messagingSenderId: "1002503639211"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				<header>
+				</header>
+				<main>
+					<Route exact path="/" component={RoomList}/>
+				</main>
+				<footer>
+				</footer>
       </div>
     );
   }
